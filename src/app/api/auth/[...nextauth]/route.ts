5983f0881ca1c4e512dcf23538c9534c8b4dc4ed
Ahmad Jaber <ahmad.jaber010@yahoo.com>
2024-11-05 14:00:14 +0100

    fix src folder

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 // /api/auth/[...nextauth]/route.ts
 
 import NextAuth from "next-auth";
-import { authOptions } from "@/src/app/lib/auth";
+import { authOptions } from "@/src/lib/auth";
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
