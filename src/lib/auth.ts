ff8f6fceceb19e5c1a4ffea18514fe23ba8411ca
Ahmad Jaber <ahmad.jaber010@yahoo.com>
2024-11-06 16:02:04 +0100

    Auth Run OK

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,15 +2,6 @@ import NextAuth from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import Nodemailer from "next-auth/providers/nodemailer"
 import { prisma } from "@/lib/prisma"
- 
-const emailHost = process.env.EMAIL_SERVER_HOST
-const emailPort = process.env.EMAIL_SERVER_PORT
-const emailUser = process.env.EMAIL_SERVER_USER
-const emailPassword = process.env.EMAIL_SERVER_PASSWORD
-const emailFrom = process.env.EMAIL_FROM
-
-console.log(emailHost, emailPort, emailUser, emailPassword, emailFrom);
-
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
